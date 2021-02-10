4f072b4ea3d1984718e1db0cc3b6bf93196992e8
Punchcode <punchcode@Punchcodes-MBP.fios-router.home>
2021-02-10 11:06:02 -0500

    final like button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,20 @@
-import {useState, useEffect} from 'react';
 
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   
-  const [counter, setCounter] = useState(0)
+  const [count, setCount] = useState(0)
   const [likes, setLikes] = useState('likes')
-  function handleClick() {
-    setCounter(counter + 1)
-    if (counter === 0){
-      setLikes('like')
-    }else{
-      setLikes('likes')
-    }
-  }
+
+  useEffect(() => count === 1 ? setLikes('like') : setLikes('likes'),[count])
+
+  //end the useEffect arg with an array of what you want the effect to watch.
+  
   return (
-    <div>
-      <button id="myButton" onClick={handleClick}>{setLikes}{counter}</button>
+    <div className="myButton">
+      <button className="btn" onClick={() => setCount(count + 1)}>{count} {likes}
+      </button>
     </div>
   );
   }
-
-  export default App;
--- my-app/src/button.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function button(props) {
-    return <button onClick={props.onClick}>click</button>
-}
